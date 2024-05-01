4f2ded8283b857156640646515149125ac31533c
samuelfsd <samuel.estudo778@gmail.com>
2024-05-01 11:32:42 -0300

    feat: config colors theme and font Poppins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Knex</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-8xl font-sans font-bold text-purple-dark">Knex</h1>
+      <p className="font-sans text-gray">the tech future</p>
     </div>
   )
 }
